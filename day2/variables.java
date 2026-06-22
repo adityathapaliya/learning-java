5c6cb49665d5798d162f4c49060526395d2aa76e
aditya thapaliya <adityathapaliya90@gmail.com>
2026-06-22 08:56:43 +0545

    learned variables and datatypes”

---
+++ day2/variables.java
@@ -0,0 +1,29 @@
+public class variables{
+    public static void main(String[] args) {
+        // variables and data type 
+        byte a = 1;
+        short b= 100;
+        char c = 'H';
+        boolean d = true;
+        int e = 1000000;
+        long f = 100000000000000000L;
+        float g = 10.21f;
+        double h = 123.432324d;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        
+        //sum
+        int sum = a + b;
+        f += e;  
+        System.out.println(sum);
+        System.out.println(f);
+        
+    }
+}
