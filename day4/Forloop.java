5a22f3399825ff7006e854425de5cd9c7f44349d
aditya thapaliya <adityathapaliya90@gmail.com>
2026-06-24 19:22:29 +0545

    Learned For loop

---
+++ day4/Forloop.java
@@ -0,0 +1,22 @@
+package day4;
+
+public class Forloop {
+    public static void main (String []args ){
+
+        // for (int i = 0 ; i <11;i++){
+        //     System.out.println(i);
+        // }
+
+        // for(; ;) {
+        //     System.out.println("loop");
+        // }
+
+        // for (int i =0; i <=20 ; i+=2){
+        //     System.out.println(i);
+        // }
+        for (int i =1; i <=20 ; i++){
+            if (i % 2 == 0)
+                System.out.println(i);            
+        }
+    }
+}
